103eb2c0d88ce3b8c66bf49a5f220c19ff819364
nkmrh <hajimeet@me.com>
2017-12-01 10:49:02 +0900

    Update

--- example/6_6.playground/Contents.swift
+++ example/6_6.playground/Contents.swift
@@ -51,35 +51,39 @@ class ViewController : UIViewController {
         tableView.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
         tableView.heightAnchor.constraint(equalToConstant: 200).isActive = true
 
-        let s = Observable.just("tok")
-
-        let suggestions = lookup(urlString: "http://gd.geobytes.com/AutoCompleteCity?q=", keyword: s).share(replay: 1)
-
-        suggestions.bind(to: tableView.rx.items(cellIdentifier: "Cell")) { row, element, cell in
-            cell.textLabel?.text = element
-        }
-        .disposed(by: disposeBag)
+        let sEntered = autocomplete(textEdit: textField, tableView: tableView)
+        lookup(urlString: "http://getcitydetails.geobytes.com/GetCityDetails?fqcn=", keyword: sEntered)
+            .subscribe(onNext: { cityInfo in
+                print("----------------------------------------")
+                print(cityInfo)
+            })
+            .disposed(by: disposeBag)
     }
 
     func currentTextOf(textField: UITextField) -> BehaviorSubject<String?> {
-        let sKeyPresses = textField.rx.controlEvent(.valueChanged)
+        let sKeyPresses = textField.rx.text
         let text = BehaviorSubject<String?>(value: textField.text)
-        sKeyPresses.map { _ in textField.text }.subscribe(text)
+        sKeyPresses.subscribe(text)
         return text
     }
 
-    func lookup(urlString: String, keyword: Observable<String>) -> Observable<[String]> {
+    func lookup(urlString: String, keyword: Observable<String?>) -> Observable<[String]> {
         return .create { [weak self] observer -> Disposable in
             guard let strongSelf = self else { return Disposables.create() }
             keyword
                 .subscribe(onNext: { keyword in
+                    guard let keyword = keyword else { return }
                     if let url = URL(string: urlString.appending(keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")) {
                         let task = URLSession.shared.dataTask(with: url) { data, response, error in
                             if let error = error {
                                 observer.onError(error)
-                            } else if let data = data,
-                                let suggestions = (try? JSONSerialization.jsonObject(with: data)) as? [String] {
-                                observer.onNext(suggestions)
+                            } else if let data = data {
+                                if let suggestions = (try? JSONSerialization.jsonObject(with: data)) as? [String] {
+                                    observer.onNext(suggestions)
+                                } else if let suggestions = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any] {
+                                    print(suggestions)
+//                                    observer.onNext(suggestions)
+                                }
                             }
                         }
                         task.resume()
@@ -90,19 +94,31 @@ class ViewController : UIViewController {
         }
     }
 
-    func autocomplete(textEdit: UITextField, tableView: UITableView) {
+    func autocomplete(textEdit: UITextField, tableView: UITableView) -> Observable<String?> {
         let editText = currentTextOf(textField: textEdit)
-        let sKeyPresses = textEdit.rx.controlEvent(.valueChanged)
-        let sDebounced = sKeyPresses.startWith(()).debounce(100, scheduler: MainScheduler.instance)
-        let sTextUpdate = sDebounced.withLatestFrom(editText) { _, text in text }.distinctUntilChanged { $0 != $1 }
-        let sEnterKey = textEdit.rx.controlEvent([.editingDidEndOnExit])
-        let sClicked = tableView.rx.modelSelected(String.self)
-        
+        let sDebounced = editText.startWith(nil).debounce(0.1, scheduler: MainScheduler.instance)
+
+        let sEnterKey = textEdit.rx.controlEvent([.editingDidEndOnExit]).map { _ in }
+        let sClicked = tableView.rx.modelSelected(String?.self).map { $0 }
+        let sClearPopUp = Observable.of(sClicked.map { _ in }, sEnterKey.map{ _ in }).merge()
+
+        let lookedUp = Observable.of(sClearPopUp.map { _ in [String]() },
+                                     lookup(urlString: "http://gd.geobytes.com/AutoCompleteCity?q=", keyword: sDebounced)
+            .map { response in
+                return (response.count == 1 && (response.first == "%s" || response.first == "")) ? [String]() : response
+            }).merge()
+
+//        lookedUp.bind(to: tableView.rx.items(cellIdentifier: "Cell")) { row, element, cell in
+//            cell.textLabel?.text = element
+//        }
+//        .disposed(by: disposeBag)
+
+        return Observable.of(sEnterKey.withLatestFrom(editText), sClicked).merge()
     }
 }
 
 let viewController = ViewController()
-let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 740, height: 1600))
+let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 768, height: 1024))
 window.rootViewController = viewController
 window.makeKeyAndVisible()
 
